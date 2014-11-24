52b03ffa08f8244083026641d3b41dc23f6656c6
ktapakorn <bossmonfire@gmail.com>
2014-11-23 23:16:14 -0800

    registration verification is done

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
     <head>
         <title>Register</title>
         <link rel="stylesheet" type="text/css" href="style/reset.css">
-        <link rel="stylesheet" type="text/css" href="style/mystyle.css">
-        <script type="text/javascript" src="script/jquery2.1.1.js" ></script>
-        <script type="text/javascript" src="script/main.js"></script>
+        <link rel="stylesheet" type="text/css" href="style/mystyle.css">  
     </head>
     <body>
         <div id="outter"> 
@@ -17,7 +15,7 @@
                     <a class="headlink" href="">Main</a>
                 </div>
                 <div id="rightheader">
-                    <a class="headlink" href="signin.html"> Sign In </a>
+                    <a class="headlink" href="signin.php"> Sign In </a>
                 </div>
             </div>
             <div id="container"> 
@@ -26,29 +24,33 @@
                         Create you account
                     </div>
                     <div id="register">
+                        <div class=""
                         <!-- This is a form for register a new account -->
-                        <form method="POST" name="inputform" action="process.php">
+                        <form method="POST" id="registerform" name="inputform" action="process.php" >
                             <!-- First Name -->
                             <div>
-                                <input type="text" name="fname" id="fname" class="rinput" placeholder="First Name"/>
+                                <input type="text" name="fname" id="fname" class="rinput" placeholder="First Name" required/>
                                 <!-- Last Name -->
-                                <input type="text" name="lname" id="lname" class="rinput" placeholder="Last Name"/>
+                                <input type="text" name="lname" id="lname" class="rinput" placeholder="Last Name" required/>
                             </div>
                             <!-- Email Address -->
                             <div>
-                                <input type="text" name="email" id="email" class="rinput" placeholder="Email Address"/>
+                                <input type="email" name="email" id="email" class="rinput" placeholder="Email Address" required/>
                             </div>
                             <!-- Password -->
                             <div>
-                                <input type="password" name="password" id="password" class="rinput" placeholder="Password"/>
+                                <input type="password" name="password" id="password" class="rinput" placeholder="Password" required/>
                             </div>
                             <!-- Confirm Password -->
                             <div>
-                                <input type="password" name="cpassword" id="cpassword" class="rinput" placeholder="Re-enter Password"/>
+                                <input type="password" name="cpassword" id="cpassword" class="rinput" placeholder="Re-enter Password" required/>
                             </div>
-<!--                            <div> 
-                                <input type="pin" id="pin" class="rinput" placeholder="4 Digit PIN number" maxlength="4"/>
-                            </div>-->
+                            <div> 
+                                <p class="passwarning"></p> 
+                            </div> 
+                            <!--                            <div> 
+                                                            <input type="pin" id="pin" class="rinput" placeholder="4 Digit PIN number" maxlength="4"/>
+                                                        </div>-->
                             <!-- Create new account button -->
                             <div id="butt">
                                 <input type="submit" name="submit" class="createacc" value="Create Account"/>
@@ -68,5 +70,10 @@
         <div id="second"> 
 
         </div>
+            
+            
+    <!--Javascript Placed at the buttom to make it load faster-->
+     <script type="text/javascript" src="script/jquery2.1.1.js" ></script> 
+     <script type="text/javascript" src="script/main.js"></script>
     </body>
 </html>
--- script/main.js
+++ script/main.js
@@ -1,7 +1,31 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(document).ready(function () {
+    var check = 0;
+    $("#cpassword").keyup(function () {
+        check = 0;
+        var p1 = $("#password").val();
+        var p2 = $("#cpassword").val();
+        if (p1 == p2)
+        {
+            $(this).css("border", "1px solid green");
+            $(".passwarning").html("Password Matches");
+            $(".passwarning").css("color", "green");
+            check = 1;
+        } else {
+            $(this).css("border", "1px solid red");
+            $(".passwarning").html("");
+            check = 0;
+        } 
+        if (!$("#cpassword").val())
+        {
+             $(this).css("border", "1px solid #c0c8c9");
+        }
+    });
+    $("#registerform").submit(function (event) {
+        if(check != 1)
+        {
+            event.preventDefault();
+            $(".passwarning").html("Password Mismatch");
+            $(".passwarning").css("color", "red");
+        }
+    });
+});
