dc91bb3095cfafacc5603d7eaeae6007189618e7
ktapakorn <bossmonfire@gmail.com>
2014-12-13 18:51:19 -0800

    update database

--- database/PVault.sql
+++ database/PVault.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2014 at 07:53 PM
+-- Generation Time: Dec 14, 2014 at 02:50 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.3.29
 
@@ -23,6 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `documents`
+--
+
+CREATE TABLE IF NOT EXISTS `documents` (
+  `uid` int(11) NOT NULL,
+`did` int(11) NOT NULL,
+  `filename` varchar(30) NOT NULL,
+  `expdate` date DEFAULT NULL,
+  `comment` varchar(50) DEFAULT NULL,
+  `uploadTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `filePath` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forgot`
+--
+
+CREATE TABLE IF NOT EXISTS `forgot` (
+  `email` varchar(50) NOT NULL,
+  `hash` char(64) NOT NULL,
+  `date` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,31 +59,29 @@ CREATE TABLE IF NOT EXISTS `users` (
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` char(64) NOT NULL,
   `active` tinyint(1) NOT NULL,
-  `pin` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+  `hash` char(64) NOT NULL,
+  `salt` char(6) NOT NULL,
+  `login_attempts` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`uid`, `firstname`, `lastname`, `email`, `password`, `active`, `pin`) VALUES
-(1, 'name1', 'lastname1', 'email1@email.com', 'password1', 1, 0),
-(3, 'John', 'Doe', 'john@example.com', 'passwrod', 0, 0),
-(4, '', '', '', '', 0, 0),
-(11, 'first', 'namd', 'dkfdkf', 'sdfsdfdfdsfsdfs', 0, 0),
-(15, 'a', 'a', 'a@a', 'aa', 0, 0),
-(16, 'aaa', 'aaa', 'aaa@aaa', 'aaa', 0, 0),
-(17, 'aaa', 'aaa', 'dfd@ae', 'aaa', 0, 0),
-(18, 'aa', 'aaa', 'aa3g32df2dfdf@dfdf', 'aaa', 0, 0),
-(20, 'ddd', 'ddd', 'ddd@ddd', 'dd', 0, 0),
-(21, 'ddd', 'ddd', 'ddd@ddde', 'aa', 0, 0),
-(22, 'ddd', 'ddd', 'ddd@dddee', 'aaa', 0, 0);
+INSERT INTO `users` (`uid`, `firstname`, `lastname`, `email`, `password`, `active`, `hash`, `salt`, `login_attempts`) VALUES
+(23, 'tapakorn', 'kittirudeekul', 'bossmonfire@gmail.com', '8f834ae62f17be72634f70cb47b2cf160ea610e5c28e2a58f7b2255d8c69bdd6', 0, '75a66b8906cfd49b9d989f1029b9d19598ba7552f2597bd8161bd57e320000be', '3fda26', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+ ADD PRIMARY KEY (`did`), ADD KEY `uid` (`uid`);
 
 --
 -- Indexes for table `users`
@@ -68,10 +94,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+MODIFY `did` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `documents`
+--
+ALTER TABLE `documents`
+ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
